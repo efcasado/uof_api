0603785eb7d3d3ea283244dc7dce63b4fa0f134d
Enrique Fernandez <enrique@bluelabs.eu>
2024-04-18 17:35:14 +0200

    chore(tests): add tests for UOF.API.tournament/1

--- test/uof_api_test.exs
+++ test/uof_api_test.exs
@@ -2,4 +2,17 @@ defmodule UofApiTest do
   use ExUnit.Case
 
   doctest UOF.API
+
+  test "should parse all available tournaments" do
+    %{tournaments: tournaments} = UOF.API.tournaments()
+    # sample 10% of all available tournaments
+    tournaments = sample(tournaments, 10)
+    Enum.each(tournaments, &UOF.API.tournament(&1.id))
+  end
+
+  def sample(xs, percent) do
+    n = div(Enum.count(xs), percent)
+
+    Enum.take(Enum.shuffle(xs), n)
+  end
 end
