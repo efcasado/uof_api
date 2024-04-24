34322ed978e5fd1a7286e3807ca934c7ae958cf3
Enrique Fernandez <enrique@bluelabs.eu>
2024-04-24 19:08:16 +0200

    feat: ability to toggle parsing of response body

--- lib/utils/http.ex
+++ lib/utils/http.ex
@@ -1,20 +1,25 @@
 defmodule UOF.API.Utils.HTTP do
   import Logger
 
-  def get(path) do
+  def get(path, schema) do
     base_url = Application.get_env(:uof_api, :base_url)
     auth_token = Application.get_env(:uof_api, :auth_token)
 
     url = Enum.join([base_url | path], "/")
 
-    Req.get!(url,
-      headers: %{"x-access-token" => auth_token}
-    ).body
-  end
+    response =
+      Req.get!(url,
+        headers: %{"x-access-token" => auth_token}
+      ).body
 
-  def get(path, schema) do
-    response = get(path)
     debug("response=#{response}")
-    Saxaboom.parse(response, schema)
+
+    case Application.get_env(:uof_api, :parse, true) do
+      true ->
+        Saxaboom.parse(response, schema)
+
+      false ->
+        response
+    end
   end
 end
