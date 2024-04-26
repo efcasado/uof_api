4370f68d01bbaf0ccccb3541b671033c62047677
Enrique Fernandez <enrique@bluelabs.eu>
2024-04-26 21:10:41 +0200

    fix: casting and parsing of fixture properties

--- lib/uof/api/mappings/fixture.ex
+++ lib/uof/api/mappings/fixture.ex
@@ -5,22 +5,22 @@ defmodule UOF.API.Mappings.Fixture do
   alias UOF.API.Mappings.{Season, Tournament, TournamentRound, TVChannel, Venue}
 
   document do
-    attribute(:start_time_confirmed)
+    attribute(:start_time_confirmed, cast: :boolean)
     attribute(:start_time)
     attribute(:liveodds)
     attribute(:status)
     attribute(:next_live_time)
     attribute(:id)
     attribute(:scheduled)
-    attribute(:start_time_tbd)
+    attribute(:start_time_tbd, cast: :boolean)
     elements(:competitor, as: :competitors, into: %Competitor{})
     elements(:tv_channel, as: :tv_channels, into: %TVChannel{})
+    elements(:info, as: :extra_info, into: %ExtraInfo{})
     elements(:reference_id, as: :references, into: %Reference{})
     element(:tournament_round, into: %TournamentRound{})
     element(:tournament, into: %Tournament{})
     element(:season, into: %Season{})
     element(:venue, into: %Venue{})
-    element(:extra_info, into: %ExtraInfo{})
     element(:coverage_info, into: %CoverageInfo{})
     element(:product_info, into: %ProductInfo{})
   end
