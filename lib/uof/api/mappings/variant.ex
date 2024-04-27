b816d38d9ddeaf958821d17282ffbd33f4191baf
Enrique Fernandez <enrique@bluelabs.eu>
2024-04-27 13:34:12 +0200

    feat: add UOF.API.variants/{0, 1}

---
+++ lib/uof/api/mappings/variant.ex
@@ -0,0 +1,11 @@
+defmodule UOF.API.Mappings.Variant do
+  use Saxaboom.Mapper
+
+  alias UOF.API.Mappings.{OutcomeMappings, Outcome}
+
+  document do
+    attribute(:id)
+    elements(:outcome, as: :outcomes, into: %Outcome{})
+    elements(:mapping, as: :mappings, into: %OutcomeMappings{})
+  end
+end
