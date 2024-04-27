c18d4a8ed1d09170083662b501b7b2a16d2246cf
Enrique Fernandez <enrique@bluelabs.eu>
2024-04-27 08:54:34 +0200

    fix: incomplete tournament info response

---
+++ lib/uof/api/mappings/round.ex
@@ -0,0 +1,10 @@
+defmodule UOF.API.Mappings.Round do
+  use Saxaboom.Mapper
+
+  document do
+      attribute :type
+      attribute :name
+      attribute :cup_round_matches, cast: :integer
+      attribute :cup_round_match_number, cast: :integer
+  end
+end
--- lib/uof/api/mappings/tournament_info.ex
+++ lib/uof/api/mappings/tournament_info.ex
@@ -1,11 +1,12 @@
 defmodule UOF.API.Mappings.TournamentInfo do
   use Saxaboom.Mapper
 
-  alias UOF.API.Mappings.{CoverageInfo, Group, Season, SeasonCoverage, Tournament}
+  alias UOF.API.Mappings.{CoverageInfo, Group, Round, Season, SeasonCoverage, Tournament}
 
   document do
     element(:tournament, into: %Tournament{})
     element(:season, into: %Season{})
+    element(:round, into: %Round{})
     element(:season_coverage, into: %SeasonCoverage{})
     element(:coverage_info, into: %CoverageInfo{})
     elements(:group, as: :groups, into: %Group{})
