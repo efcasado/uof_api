ab8c11fadc9c6c845534f6769eaef5d8f7cd3fbd
Enrique Fernandez <enrique@bluelabs.eu>
2024-04-27 08:54:19 +0200

    feat: add tests for mappings

--- test/uof_api_test.exs
+++ test/uof_api_test.exs
@@ -114,19 +114,6 @@ defmodule UOF.API.Test do
     assert producer.stateful_recovery_window_in_minutes == 600
   end
 
-  test "can parse 'sports/:lang/sports.xml' response" do
-    data = File.read!("test/data/sports.xml")
-
-    {:ok, %UOF.API.Mappings.Sports{sports: sports}} =
-      Saxaboom.parse(data, %UOF.API.Mappings.Sports{})
-
-    sport = hd(sports)
-
-    assert Enum.count(sports) == 204
-    assert sport.id == "sr:sport:143"
-    assert sport.name == "7BallRun"
-  end
-
   ## Static sport event information
   ##=========================================================================
 
@@ -361,6 +348,10 @@ defmodule UOF.API.Test do
     assert category.country_code == "ENG"
   end
 
+  test "can parse 'sports/:lang/sports/tournaments.xml' response" do
+    # TO-DO
+  end
+
   test "can parse 'sports/:lang/sports/:sport/tournaments.xml' response" do
     data = File.read!("test/data/tournaments.xml")
 
@@ -395,5 +386,74 @@ defmodule UOF.API.Test do
     assert season_coverage.max_covered == 136
     assert season_coverage.max_coverage_level == "gold"
     assert season_coverage.min_coverage_level == "silver"
+  end
+
+  test "can parse 'sports/:lang/sports.xml' response" do
+    data = File.read!("test/data/sports.xml")
+
+    {:ok, %UOF.API.Mappings.Sports{sports: sports}} =
+      Saxaboom.parse(data, %UOF.API.Mappings.Sports{})
+
+    sport = hd(sports)
+
+    assert Enum.count(sports) == 204
+    assert sport.id == "sr:sport:143"
+    assert sport.name == "7BallRun"
+  end
+
+  test "can parse 'sports/:lang/tournaments/:tournament/info.xml' response" do
+    data = File.read!("test/data/tournament_info.xml")
+
+    {:ok, info} = Saxaboom.parse(data, %UOF.API.Mappings.TournamentInfo{})
+
+    # tournament
+    assert info.tournament.id == "sr:tournament:7"
+    assert info.tournament.name == "UEFA Champions League"
+    # tournament -> sport
+    assert info.tournament.sport.id == "sr:sport:1"
+    assert info.tournament.sport.name == "Soccer"
+    # tournament -> category
+    assert info.tournament.category.id == "sr:category:393"
+    assert info.tournament.category.name == "International Clubs"
+    # tournament -> current season
+    assert info.tournament.current_season.start_date == "2023-06-27"
+    assert info.tournament.current_season.end_date == "2024-06-01"
+    assert info.tournament.current_season.year == "23/24"
+    assert info.tournament.current_season.id == "sr:season:106479"
+    # season
+    assert info.season.start_date == "2023-06-27"
+    assert info.season.end_date == "2024-06-01"
+    assert info.season.year == "23/24"
+    assert info.season.id == "sr:season:106479"
+    assert info.season.name == "UEFA Champions League 23/24"
+    # round
+    assert info.round.type == "cup"
+    assert info.round.name == "Quarterfinal"
+    assert info.round.cup_round_matches == 2
+    assert info.round.cup_round_match_number == 2
+    # coverage info
+    assert info.coverage_info.live_coverage == true
+    # groups
+    group = hd(info.groups)
+    assert Enum.count(info.groups) == 9
+    assert group.name == "A"
+    assert group.id == "sr:group:74525"
+    # group -> competitors
+    assert Enum.count(group.competitors) == 4
+    competitor = hd(group.competitors)
+    assert competitor.id == "sr:competitor:2672"
+    assert competitor.name == "Bayern Munich"
+    assert competitor.abbreviation == "BMU"
+    assert competitor.country == "Germany"
+    assert competitor.country_code == "DEU"
+    assert competitor.gender == "male"
+    [reference] = competitor.references
+    assert reference.name == "betradar"
+    assert reference.value == "6631"
+
+  end
+
+  test "can parse 'sports/:lang/tournaments/:tournament/seasons.xml' response" do
+    # TO-DO
   end
 end
