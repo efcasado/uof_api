0c9d85fd05b61e515e45e648bd0ecd2072eedb14
Enrique Fernandez <enrique@bluelabs.eu>
2024-04-28 19:56:06 +0200

    feat: add support for custombet api

--- lib/uof/api/utils/http.ex
+++ lib/uof/api/utils/http.ex
@@ -24,4 +24,33 @@ defmodule UOF.API.Utils.HTTP do
         response
     end
   end
+
+  def post(schema, path, body) do
+    base_url = Application.get_env(:uof_api, :base_url)
+    auth_token = Application.get_env(:uof_api, :auth_token)
+
+    url = Enum.join([base_url | path], "/")
+
+    Logger.debug(body)
+
+    response =
+      Req.post!(url,
+        headers: %{
+          "x-access-token" => auth_token,
+          "content-type" => "application/xml",
+          "accept" => "application/xml"
+        },
+        body: body
+      ).body
+
+    debug("response=#{response}")
+
+    case Application.get_env(:uof_api, :parse, true) do
+      true ->
+        Saxaboom.parse(response, schema)
+
+      false ->
+        response
+    end
+  end
 end
