2cf31e62c2b664e3168e892fa35de2b845d416c0
Enrique Fernández <enrique@bluelabs.eu>
2026-06-20 18:14:10 +0200

    feat: add support for booking events

---
+++ test/uof/api/booking_test.exs
@@ -0,0 +1,20 @@
+defmodule UOF.API.Booking.Test do
+  use ExUnit.Case
+  use Mimic
+
+  setup do
+    stub(UOF.API.Utils.HTTP, :post, fn schema, _endpoint ->
+      data = File.read!("test/data/booking_response.xml")
+      UOF.API.XML.decode(data, schema)
+    end)
+
+    :ok
+  end
+
+  test "book/1 books a sport event for live odds" do
+    {:ok, response} = UOF.API.Booking.book("sr:match:12345")
+
+    assert response.response_code == "OK"
+    assert response.action == "Event booked"
+  end
+end
