f187e3af60da4cec9559b295aa81ad224ecf8c42
Enrique Fernández <enrique@bluelabs.eu>
2026-06-20 20:04:33 +0200

    refactor: idiomatic req usage

--- lib/uof/api/utils/http.ex
+++ lib/uof/api/utils/http.ex
@@ -1,57 +1,28 @@
 defmodule UOF.API.Utils.HTTP do
   @moduledoc false
-  import Logger
 
   def get(schema, path, params \\ []) do
-    base_url = Application.get_env(:uof_api, :base_url)
-    auth_token = Application.get_env(:uof_api, :auth_token)
-
-    url = Enum.join([base_url | path], "/")
-
-    response =
-      Req.get!(url,
-        headers: %{"x-access-token" => auth_token},
-        params: params
-      ).body
-
-    debug("response=#{response}")
-
-    case Application.get_env(:uof_api, :parse, true) do
-      true ->
-        UOF.API.XML.decode(response, schema)
-
-      false ->
-        response
-    end
+    client()
+    |> Req.get!(url: Enum.join(path, "/"), params: params)
+    |> decode(schema)
   end
 
   def post(schema, path, body \\ "", params \\ []) do
-    base_url = Application.get_env(:uof_api, :base_url)
-    auth_token = Application.get_env(:uof_api, :auth_token)
-
-    url = Enum.join([base_url | path], "/")
-
-    Logger.debug(body)
-
-    response =
-      Req.post!(url,
-        headers: %{
-          "x-access-token" => auth_token,
-          "content-type" => "application/xml",
-          "accept" => "application/xml"
-        },
-        body: body,
-        params: params
-      ).body
-
-    debug("response=#{response}")
-
-    case Application.get_env(:uof_api, :parse, true) do
-      true ->
-        UOF.API.XML.decode(response, schema)
-
-      false ->
-        response
-    end
+    client()
+    |> Req.post!(url: Enum.join(path, "/"), params: params, body: body)
+    |> decode(schema)
   end
+
+  defp client do
+    Req.new(
+      base_url: Application.fetch_env!(:uof_api, :base_url) <> "/",
+      headers: %{
+        "x-access-token" => Application.fetch_env!(:uof_api, :auth_token),
+        "content-type" => "application/xml",
+        "accept" => "application/xml"
+      }
+    )
+  end
+
+  defp decode(%Req.Response{body: body}, schema), do: UOF.API.XML.decode(body, schema)
 end
