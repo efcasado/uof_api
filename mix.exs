784e6559ca37d2b99a5440f45926e9ba20f1ad3b
Enrique Fernández <enrique@bluelabs.eu>
2026-06-23 20:47:44 +0200

    test: coverage report using coveralls

--- mix.exs
+++ mix.exs
@@ -7,12 +7,25 @@ defmodule UofApi.MixProject do
       version: "2.0.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
       description: description(),
       package: package(),
       docs: docs(),
       deps: deps(),
       name: "UOF_API",
       source_url: "https://github.com/efcasado/uof_api"
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.github": :test
+      ]
     ]
   end
 
@@ -29,7 +42,9 @@ defmodule UofApi.MixProject do
       {:xml_builder, "~> 2.3"},
       # dev
       {:ex_doc, "~> 0.31", only: :dev, runtime: false},
-      {:mimic, "~> 2.3", only: :test}
+      {:mimic, "~> 2.3", only: :test},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:junit_formatter, "~> 3.3", only: :test}
     ]
   end
 
